b860aa69f9c790f3a7d1a971dd7f6d5e1e9ac434
Marcin <marcin.barylski@tas-moto.org>
2018-04-02 17:03:22 +0200

    Fixing A301776 internals

--- A301776/A301776.cpp
+++ A301776/A301776.cpp
@@ -14,17 +14,19 @@ bool is_prime (long long unsigned int n);
 
 set<long long unsigned int> set_to_be_verified;
 set<long long unsigned int> set_already_verified;
-long long unsigned int num_where_all_verified = 0;
+long long unsigned int num_where_all_verified = 4;
 
 int main()
 {
-	long long unsigned int ip1 = 0;
+	long long unsigned int ip1 = 1;
 	long long unsigned int ip2 = 0;
 	long long unsigned int p1 = 0;
 	long long unsigned int p2 = 0;
 	long long unsigned int num = 0;
 	
 	cout << "Press <Enter> to stop calculations..." << endl;
+	// first term is given fo free
+	cout << 2 << ", ";
 	while (1)
 	{	
 		p1 = get_ith_prime (ip1);
@@ -37,56 +39,89 @@ int main()
             remove_nums_to_be_verified (num, 2*p1);
 		}
 
-        if (set_to_be_verified.empty())
+        if ( set_to_be_verified.empty() )
 		{
 			cout << p1 << ", ";
 		}
 		
 		ip1++;
 		
-		if(_kbhit())
+		if (_kbhit())
 		{
-			cout << endl << "Breaking calculations at" << ip1 << endl;
+			cout << endl << "Breaking calculations at " << ip1 << endl;
             break;
         }
 	}
 	return 0;
 }
 
+// Add number to a set of numbers to be verified 
+// if number is above threshold where all numbers were already verified
 void add_nums_to_be_verified (long long unsigned int num)
 {
     if (num > num_where_all_verified)
 	{
 		for (long long unsigned int k = num_where_all_verified + 2; k <= num; k = k + 2)
 		{
-			set<long long unsigned int>::iterator it;
-			if (! (it != set_already_verified.end()) )
+			auto search = set_already_verified.find(k);
+			if (search != set_already_verified.end())
 			{
-				set_already_verified.insert(k);
+				// found, do nothing
+			}
+			else
+			{
+				// not found
+				set_to_be_verified.insert(k);
 			}
 		}
 	}
 }
 
+// Remove number from a set of already verified numbers
 void remove_nums_to_be_verified (long long unsigned int num, long long unsigned int max_num)
-{
-	/*
+{	
 	set_already_verified.insert(num);
 
-    if num in to_be_verified:
-        to_be_verified.remove(num)
-    if (len(to_be_verified) > 0):
-        min_num = min(to_be_verified)
-        if min_num > num_where_all_verified + 2:
-            num_where_all_verified = min_num
-    else:
-        num_where_all_verified = max_num
-
-    copy_already_verified = already_verified.copy()
-    for n in copy_already_verified:
-        if n < num_where_all_verified:
-            already_verified.remove(n)
-	*/
+	auto search = set_to_be_verified.find(num);
+	if ( search != set_to_be_verified.end() )
+	{
+		set_to_be_verified.erase(search);
+	}
+	
+    // update number below which all numbers were already verified
+	long long unsigned int min_num = 0;
+	if ( !set_to_be_verified.empty() )
+	{
+		set<long long unsigned int>::iterator it;
+		for (it = set_to_be_verified.begin(); it != set_to_be_verified.end(); ++it)
+		{
+			if (min_num == 0) min_num = *it;
+			else if (min_num > *it) min_num = *it;
+		}
+		
+		if (min_num > num_where_all_verified + 2)
+		{
+			num_where_all_verified = min_num;
+		}
+	}
+	else
+	{
+		num_where_all_verified = max_num;
+	}
+	
+	// remove all numbers below num_where_all_verified from further analysis
+	set<long long unsigned int>::iterator itt = set_already_verified.begin();
+	for (itt = set_already_verified.begin(); itt != set_already_verified.end(); ) 
+	{
+		if (*itt < num_where_all_verified)
+		{
+			itt = set_already_verified.erase(itt++);
+		}
+		else
+		{
+			++itt;
+		}
+	}
 }
 
 // returns ith prime
